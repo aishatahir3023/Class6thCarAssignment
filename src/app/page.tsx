07afd0e945294fdf8152f0bc3a1c16a657a3a995
aishatahir3023 <160772524+aishatahir3023@users.noreply.github.com>
2024-11-06 04:24:25 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+
+
+import React from 'react';
+import Hero from './components/hero';
+import Featuring from './components/Featuring';
+
+
+
+export default function  Home() {
+  return (
+    <div> 
+      <Hero />
+      <Featuring />  
+    </div>
+
+    
+  
+  )
+}
